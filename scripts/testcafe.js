26ec13b74c298c179b578fe0404ef022cdd9bfd3
erwanledoux <lawanledoux@gmail.com>
2019-05-24 16:41:24 +0200

    (pC-1709) try parallel bash testcafe jobs

--- scripts/testcafe.js
+++ scripts/testcafe.js
@@ -1,4 +1,4 @@
-// const fs = require('fs')
+const fs = require('fs')
 const path = require('path')
 
 exports.getRunOneTestcafeFileCommand = ({
@@ -12,26 +12,35 @@ exports.getRunOneTestcafeFileCommand = ({
   const debugOption = debug ? '-d' : ''
   const testcafeBinDir = path.join(fileDir, '../node_modules/.bin/testcafe')
   const testcafeOptions =
-    '--assertion-timeout 5000 --selector-timeout 15000 -c 4'
+    '--assertion-timeout 5000 --selector-timeout 15000 -c 2'
   const testcafeFileDir = path.join(fileDir, '../testcafe', file)
   return `NODE_ENV=${NODE_ENV} ${testcafeBinDir} ${browser} ${debugOption}\
 ${testcafeOptions} ${testcafeFileDir}`
 }
 
-/*
 exports.getRunAllTestcafeFilesCommand = config => {
   const { fileDir } = config
-  const testFileNames = fs.readdirSync(path.join(fileDir, '../testcafe'))
-                          .filter(dirName => dirName !== "helpers")
-  const command = testFileNames.map(testFileName =>
+  const workersCount = config.workersCount || 2
+  const testFileNames = fs
+    .readdirSync(path.join(fileDir, '../testcafe'))
+    .filter(dirName => dirName !== 'helpers')
+  const fileCommands = testFileNames.map(testFileName =>
     exports.getRunOneTestcafeFileCommand(
-      Object.assign({}, config, { file: testFileName })))
-                                .join('&')
+      Object.assign({}, config, { file: testFileName })
+    )
+  )
+
+  const commandsCountPerWorker = fileCommands.length / workersCount
+
+  const command = [...Array(workersCount).keys()]
+    .map(workerIndex =>
+      fileCommands
+        .slice(workerIndex, (workerIndex + 1) * commandsCountPerWorker)
+        .join(' && ')
+    )
+    .join('&')
+
   return `${command}&`
-}
-*/
-exports.getRunAllTestcafeFilesCommand = config => {
-  return exports.getRunOneTestcafeFileCommand(config, { file: '' })
 }
 
 exports.getRunTestcafeCommand = config => {
