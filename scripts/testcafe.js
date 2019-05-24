1908bf8780113f2f154f490bb76b2c4a5413e6ed
erwanledoux <lawanledoux@gmail.com>
2019-05-24 18:02:03 +0200

    (pC-1709) simple concurrently

--- scripts/testcafe.js
+++ scripts/testcafe.js
@@ -1,7 +1,6 @@
-const fs = require('fs')
 const path = require('path')
 
-exports.getRunOneTestcafeFileCommand = ({
+exports.getRunTestcafeCommand = ({
   browser,
   debug,
   environment,
@@ -15,38 +14,5 @@ exports.getRunOneTestcafeFileCommand = ({
     '--assertion-timeout 5000 --selector-timeout 15000 -c 2'
   const testcafeFileDir = path.join(fileDir, '../testcafe', file)
   return `NODE_ENV=${NODE_ENV} ${testcafeBinDir} ${browser} ${debugOption}\
-${testcafeOptions} ${testcafeFileDir}`
-}
-
-exports.getRunAllTestcafeFilesCommand = config => {
-  const { fileDir } = config
-  const workersCount = config.workersCount || 2
-  const testFileNames = fs
-    .readdirSync(path.join(fileDir, '../testcafe'))
-    .filter(dirName => dirName !== 'helpers')
-  const fileCommands = testFileNames.map(testFileName =>
-    exports.getRunOneTestcafeFileCommand(
-      Object.assign({}, config, { file: testFileName })
-    )
-  )
-
-  const commandsCountPerWorker = fileCommands.length / workersCount
-
-  const command = [...Array(workersCount).keys()]
-    .map(workerIndex =>
-      fileCommands
-        .slice(workerIndex, (workerIndex + 1) * commandsCountPerWorker)
-        .join(' && ')
-    )
-    .join('&')
-
-  return `${command}&wait`
-}
-
-exports.getRunTestcafeCommand = config => {
-  const { file } = config
-  if (file) {
-    return exports.getRunOneTestcafeFileCommand(config)
-  }
-  return exports.getRunAllTestcafeFilesCommand(config)
+  ${testcafeOptions} ${testcafeFileDir}`
 }
