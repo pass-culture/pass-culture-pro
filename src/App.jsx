67738b31f6a100272805657318eb73ef0caa17ec
Anne-Marie <anne-marie.esteves@beta.gouv.fr>
2019-08-20 15:15:48 +0200

    (PC-2137) move matomo to component app where it can have acces to user and add a new tracking for user id

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
 import classnames from 'classnames'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
-import { compose } from 'redux'
 
-const App = ({ modalOpen, children }) => {
-  return <div className={classnames('app', { 'modal-open': modalOpen })}>{children}</div>
+import MatomoPageTracker from './components/matomo/MatomoPageTracker'
+
+const App = ({ modalOpen, children, user }) => {
+  const currentUser = user
+  return (
+    <React.Fragment>
+      <div className={classnames('app', { 'modal-open': modalOpen })}>
+        {children}
+      </div>
+      <MatomoPageTracker user={currentUser} />
+    </React.Fragment>
+  )
 }
 
 function mapStateToProps(state) {
   return {
     modalOpen: state.modal.isActive,
+    user: state.user,
   }
+}
+
+App.defaultProps = {
+  user: {},
 }
 
 App.propTypes = {
   children: PropTypes.arrayOf(PropTypes.shape()).isRequired,
   modalOpen: PropTypes.bool.isRequired,
+  user: PropTypes.shape,
 }
 
 export default compose(
