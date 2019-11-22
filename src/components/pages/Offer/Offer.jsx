70d48dc0d500ae9429214d79bfafcf4e9172ea0a
aliochka <romain.chaffal@gmail.com>
2019-11-22 17:55:17 +0100

    (PC-2502) find type on offer instead of product

--- src/components/pages/Offer/Offer.jsx
+++ src/components/pages/Offer/Offer.jsx
@@ -808,7 +808,7 @@ Offer.propTypes = {
   isFeatureActive: PropTypes.bool,
   location: PropTypes.shape().isRequired,
   query: PropTypes.shape().isRequired,
-  selectedOfferType: PropTypes.arrayOf().isRequired,
+  selectedOfferType: PropTypes.shape().isRequired,
   trackCreateOffer: PropTypes.func.isRequired,
   trackModifyOffer: PropTypes.func.isRequired,
   venues: PropTypes.arrayOf(PropTypes.shape()),
