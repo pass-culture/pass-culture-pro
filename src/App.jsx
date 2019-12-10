aa10a2b46c40e6bbacc9be4f209579feb91abd84
Ahmed <aalaoui@octo.com>
2019-12-10 14:29:05 +0100

    (pc-2587): create app container and redirectMaintenance component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,18 @@
 import classnames from 'classnames'
-import { compose } from 'redux'
-import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { Redirect, withRouter } from 'react-router'
-import { URL_FOR_MAINTENANCE } from "./utils/config"
+import RedirectToMaintenance from './components/RedirectToMaintenance'
 
 export const App = ({ modalOpen, isMaintenanceActivated, children }) => {
-  console.log({URL_FOR_MAINTENANCE})
   if (isMaintenanceActivated) {
-    return (<Redirect to={URL_FOR_MAINTENANCE} />)
-  } else
-    return (
-      <div className={classnames('app', { 'modal-open': modalOpen })}>
-        {children}
-      </div>
-    )
-}
-
-
-function mapStateToProps(state) {
-  return {
-    modalOpen: state.modal.isActive,
-  }
+    return <RedirectToMaintenance />
+  } else return (<div className={classnames('app', { 'modal-open': modalOpen })}>
+    {children}
+  </div>)
 }
 
 App.propTypes = {
   children: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  isMaintenanceActivated: PropTypes.bool.isRequired,
   modalOpen: PropTypes.bool.isRequired,
 }
-
-export default compose(
-  withRouter,
-  connect(mapStateToProps)
-)(App)
