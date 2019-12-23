d755a82163fd3f39acc17c2b66999a7ba300832b
Brandone Martins <brm@octo.com>
2019-12-23 16:54:10 +0100

    (MOB) change store configuration for tests (from saga to thunk -data)

--- src/components/hocs/with-login/__specs__/configure.js
+++ src/components/hocs/with-login/__specs__/configure.js
@@ -1,27 +1,16 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux'
-import createSagaMiddleware from 'redux-saga'
-import { all } from 'redux-saga/effects'
-import { createDataReducer, watchDataActions } from 'redux-thunk-data'
+import { createDataReducer } from 'redux-thunk-data'
+import thunk from 'redux-thunk'
 
-export const configureTestStore = () => {
-  const sagaMiddleware = createSagaMiddleware()
-  const storeEnhancer = applyMiddleware(sagaMiddleware)
 
-  function* rootSaga() {
-    yield all([
-      watchDataActions({
-        rootUrl: 'http://foo.com',
-      }),
-    ])
-  }
+export function configureTestStore() {
+  const storeEnhancer = applyMiddleware(thunk.withExtraArgument({ rootUrl: 'http://foo.com' }))
 
   const rootReducer = combineReducers({
     data: createDataReducer({ users: [] }),
   })
 
   const store = createStore(rootReducer, storeEnhancer)
-
-  sagaMiddleware.run(rootSaga)
 
   return store
 }
