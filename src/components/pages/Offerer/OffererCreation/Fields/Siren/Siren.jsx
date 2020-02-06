11f010f4e51081678df69b1d4fc0ca1e1fc583b5
Brandone Martins <brm@octo.com>
2020-02-06 10:54:40 +0100

    (MOB): Add SIREN validation on SIREN Field

--- src/components/pages/Offerer/OffererCreation/Fields/Siren/Siren.jsx
+++ src/components/pages/Offerer/OffererCreation/Fields/Siren/Siren.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { Field } from 'react-final-form'
 import FieldErrors from '../../../../../layout/form/FieldErrors'
 import formatSiren from './formatSiren'
-import { composeValidators } from 'react-final-form-utils'
+import { composeValidators, removeWhitespaces } from 'react-final-form-utils'
+import getSirenInformation from '../../decorators/getSirenInformation'
 
 const required = value => {
   return value ? undefined : 'Ce champs est obligatoire'
@@ -12,12 +13,20 @@ const mustHaveTheProperLength = value => {
   return value.length < 11 ? 'SIREN trop court' : undefined
 }
 
+export const existsInINSEERegistry = async value => {
+  value = removeWhitespaces(value)
+  const sirenInformation = await getSirenInformation(value)
+  if (sirenInformation.error)
+    return 'Ce SIREN n\'est pas reconnu'
+  return undefined
+}
+
 const Siren = () => (
   <Field
     format={formatSiren}
     minLength={11}
     name="siren"
-    validate={composeValidators(required, mustHaveTheProperLength)}
+    validate={composeValidators(required, mustHaveTheProperLength, existsInINSEERegistry)}
   >
     {({ input, meta }) => {
       return (
