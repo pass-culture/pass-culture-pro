30a059f63bdc8979696834882c19405debd8fa6c
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-03-05 10:12:03 +0100

    (MOB): WIP - Rewrite password input

--- src/components/pages/Signup/SignupForm/Fields/Password.jsx
+++ src/components/pages/Signup/SignupForm/Fields/Password.jsx
@@ -2,6 +2,7 @@ import { Field } from "react-final-form"
 import React from "react"
 import isRequired from '../validators/isRequired'
 import { composeValidators } from 'react-final-form-utils'
+import classnames from 'classnames'
 
 function Password() {
   return (
@@ -11,7 +12,7 @@ function Password() {
     >
       {({ input, meta }) => {
         return (
-          <div className="field field-password with-subtitle">
+          <div className="field has-addons field-password with-subtitle">
             <label
               className="label"
               htmlFor="user-password"
@@ -23,7 +24,7 @@ function Password() {
                 {'... pour se connecter'}
               </p>
             </label>
-            <div className="control">
+            <div className="control is-expanded">
               <input
                 aria-describedby="user-email-error"
                 className="input is-normal"
@@ -33,6 +34,16 @@ function Password() {
                 required
                 {...input}
               />
+            </div>
+            <div className="control with-info">
+              <button
+                className="button is-rounded"
+                /*onClick={this.toggleHidden}*/
+                type="button"
+              >
+                <img alt="ico-eye" src="/icons/ico-eye.svg" />
+                &nbsp;
+              </button>
             </div>
             {meta.error && meta.touched && (
               <ul
@@ -62,3 +73,87 @@ function Password() {
 }
 
 export default Password
+
+/*
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+import BasicInput from './BasicInput'
+import { Icon } from '../Icon'
+
+class PasswordInput extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isPasswordHidden: true,
+    }
+  }
+
+  toggleHidden = e => {
+    e.preventDefault()
+    this.setState(previousState => ({
+      isPasswordHidden: !previousState.isPasswordHidden,
+    }))
+  }
+
+  onInputChange = event => {
+    const { onChange: fieldOnChange } = this.props
+    event.persist()
+    fieldOnChange(event.target.value, { event })
+  }
+
+  render() {
+    const { noPasswordToggler, info, ...otherProps } = this.props
+    const { isPasswordHidden } = this.state
+
+    const input = (
+      <BasicInput
+        {...otherProps}
+        type={isPasswordHidden ? 'password' : 'text'}
+        onChange={this.onInputChange}
+      />
+    )
+    if (noPasswordToggler) return input
+    return (
+      <div className="field has-addons field-password">
+        <div className="control is-expanded">{input}</div>
+        <div className={classnames("control", "with-info")}>
+          <button
+            className="button is-rounded"
+            onClick={this.toggleHidden}
+            type="button"
+          >
+            <Icon
+              svg={isPasswordHidden ? 'ico-eye close' : 'ico-eye'}
+            />
+            &nbsp;
+          </button>
+        </div>
+        {info && (
+          <span
+            className='column is-2'
+            data-place='bottom'
+            data-tip={info}
+            data-type='info'
+          >
+            <Icon svg="picto-info" />
+          </span>
+        )}
+      </div>
+    )
+  }
+}
+
+PasswordInput.defaultProps = {
+  info: null,
+  noPasswordToggler: false
+}
+
+PasswordInput.propTypes = {
+  info: PropTypes.string,
+  noPasswordToggler: PropTypes.bool
+}
+
+export default PasswordInput
+*/
