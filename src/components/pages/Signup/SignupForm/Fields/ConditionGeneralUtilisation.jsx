1f7459113f326638d0269d4f98a9e4df38f3c114
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-03-12 11:11:42 +0100

    (MOB): Turn components into Field components

--- src/components/pages/Signup/SignupForm/Fields/ConditionGeneralUtilisation.jsx
+++ src/components/pages/Signup/SignupForm/Fields/ConditionGeneralUtilisation.jsx
@@ -1,34 +1,42 @@
 import React from "react"
+import { Field } from 'react-final-form'
 
 function ConditionGeneralUtilisation() {
   return (
-    <div
-      className="field field-checkbox cgu-field"
+    <Field
+      name="cgu_ok"
     >
-      <label
-        className="required"
-        htmlFor="user-cgu_ok"
-      >
-        <input
-          aria-describedby="user-cgu_ok-error"
-          className="input"
-          id="user-cgu_ok"
-          name="cgu_ok"
-          required=""
-          type="checkbox"
-          value=""
-        />
-        {'J’ai lu et j’accepte les '}
-        <a
-          href="https://docs.passculture.app/textes-normatifs"
-          id="accept-cgu-link"
-          rel="noopener noreferrer"
-          target="_blank"
+      {({ input }) => (
+        <div
+          className="field field-checkbox cgu-field"
         >
-          {'Conditions Générales d’Utilisation'}
-        </a>
-      </label>
-    </div>
+          <label
+            className="required"
+            htmlFor="user-cgu_ok"
+          >
+            <input
+              {...input}
+              aria-describedby="user-cgu_ok-error"
+              className="input"
+              id="user-cgu_ok"
+
+              required=""
+              type="checkbox"
+              value=""
+            />
+            {'J’ai lu et j’accepte les '}
+            <a
+              href="https://docs.passculture.app/textes-normatifs"
+              id="accept-cgu-link"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              {'Conditions Générales d’Utilisation'}
+            </a>
+          </label>
+        </div>
+      )}
+    </Field>
   )
 }
 
--- src/components/pages/Signup/SignupForm/Fields/JoinSurvey.jsx
+++ src/components/pages/Signup/SignupForm/Fields/JoinSurvey.jsx
@@ -1,23 +1,30 @@
 import React from "react"
+import { Field } from 'react-final-form'
 
 function JoinSurvey() {
   return (
-    <div className="field field-checkbox">
-      <label
-        className=""
-        htmlFor="user-contact_ok"
-      >
-        <input
-          aria-describedby="user-contact_ok-error"
-          className="input"
-          id="user-contact_ok"
-          name="contact_ok"
-          type="checkbox"
-          value=""
-        />
-        {'J’accepte d\'être contacté par e-mail pour donner mon avis sur le pass Culture'}
-      </label>
-    </div>
+    <Field
+      name="contact"
+    >
+      {({ input }) => (
+        <div className="field field-checkbox">
+          <label
+            className=""
+            htmlFor="user-contact_ok"
+          >
+            <input
+              {...input}
+              aria-describedby="user-contact_ok-error"
+              className="input"
+              id="user-contact_ok"
+              type="checkbox"
+              value=""
+            />
+            {'J’accepte d\'être contacté par e-mail pour donner mon avis sur le pass Culture'}
+          </label>
+        </div>
+      )}
+    </Field>
   )
 }
 
