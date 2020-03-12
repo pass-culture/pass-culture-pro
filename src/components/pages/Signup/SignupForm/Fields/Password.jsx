8b353da1632e4dc56398713162c8a5867703f953
Brandone Martins <brandone.martins@gmail.com>
2020-03-12 12:07:09 +0100

    (MOB): style password component on signup page

--- src/components/pages/Signup/SignupForm/Fields/Password.jsx
+++ src/components/pages/Signup/SignupForm/Fields/Password.jsx
@@ -1,159 +1,116 @@
-import { Field } from "react-final-form"
-import React from "react"
+import { Field } from 'react-final-form'
+import React, { PureComponent } from 'react'
 import isRequired from '../validators/isRequired'
 import { composeValidators } from 'react-final-form-utils'
-import classnames from 'classnames'
+import Tooltip from '../../../../layout/Tooltip'
 
-function Password() {
-  return (
-    <Field
-      name="password"
-      validate={composeValidators(isRequired)}
-    >
-      {({ input, meta }) => {
-        return (
-          <div className="field has-addons field-password with-subtitle">
-            <label
-              className="label"
-              htmlFor="user-password"
-            >
-              <h3 className="required">
-                {'Mot de passe'}
-              </h3>
-              <p>
-                {'... pour se connecter'}
-              </p>
-            </label>
-            <div className="control is-expanded">
-              <input
-                aria-describedby="user-email-error"
-                className="input is-normal"
-                id="user-password"
-                name="password"
-                placeholder="Mon mot de passe"
-                required
-                {...input}
-              />
-            </div>
-            <div className="control with-info">
-              <button
-                className="button is-rounded"
-                /*onClick={this.toggleHidden}*/
-                type="button"
-              >
-                <img alt="ico-eye" src="/icons/ico-eye.svg" />
-                &nbsp;
-              </button>
-            </div>
-            {meta.error && meta.touched && (
-              <ul
-                className="help is-danger"
-                id="user-email-error"
-              >
-                <p
-                  className="help is-danger columns"
-                  id="user-email-error"
-                >
-                  <span className="column is-narrow is-vcentered">
-                    <img
-                      alt="Attention"
-                      src="/icons/picto-warning.svg"
-                    />
-                  </span>
-                  <span className="column is-paddingless is-narrow">
-                    {meta.error}
-                  </span>
-                </p>
-              </ul>
-            )}
-          </div>
-        )
-      }}
-    </Field>)
-}
-
-export default Password
-
-/*
-import classnames from 'classnames'
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-
-import BasicInput from './BasicInput'
-import { Icon } from '../Icon'
-
-class PasswordInput extends Component {
+class Password extends PureComponent {
   constructor(props) {
     super(props)
     this.state = {
-      isPasswordHidden: true,
+      hiddenPassword: true,
     }
   }
 
-  toggleHidden = e => {
-    e.preventDefault()
-    this.setState(previousState => ({
-      isPasswordHidden: !previousState.isPasswordHidden,
+  handleToggleHidden = () => {
+    this.setState(state => ({
+      hiddenPassword: !state.hiddenPassword,
     }))
   }
 
-  onInputChange = event => {
-    const { onChange: fieldOnChange } = this.props
-    event.persist()
-    fieldOnChange(event.target.value, { event })
-  }
-
   render() {
-    const { noPasswordToggler, info, ...otherProps } = this.props
-    const { isPasswordHidden } = this.state
+    const { hiddenPassword } = this.state
 
-    const input = (
-      <BasicInput
-        {...otherProps}
-        type={isPasswordHidden ? 'password' : 'text'}
-        onChange={this.onInputChange}
-      />
-    )
-    if (noPasswordToggler) return input
     return (
-      <div className="field has-addons field-password">
-        <div className="control is-expanded">{input}</div>
-        <div className={classnames("control", "with-info")}>
-          <button
-            className="button is-rounded"
-            onClick={this.toggleHidden}
-            type="button"
-          >
-            <Icon
-              svg={isPasswordHidden ? 'ico-eye close' : 'ico-eye'}
-            />
-            &nbsp;
-          </button>
-        </div>
-        {info && (
-          <span
-            className='column is-2'
-            data-place='bottom'
-            data-tip={info}
-            data-type='info'
-          >
-            <Icon svg="picto-info" />
-          </span>
-        )}
-      </div>
+      <Field
+        name="password"
+        type={hiddenPassword ? 'password' : 'text'}
+        validate={composeValidators(isRequired)}
+      >
+        {({ input, meta }) => {
+          return (
+            <div className="field field-password with-subtitle">
+              <label
+                className="label"
+                htmlFor="user-password"
+              >
+                <h3 className="required">
+                  {'Mot de passe'}
+                </h3>
+                <p>
+                  {'... pour se connecter'}
+                </p>
+              </label>
+              <div className="input-container">
+                <input
+                  className="password-input"
+                  id="user-password"
+                  name="password"
+                  placeholder="Mon mot de passe"
+                  required
+                  type="password"
+                  {...input}
+                />
+                <button
+                  className=""
+                  onClick={this.handleToggleHidden}
+                  type="button"
+                >
+                  <img
+                    alt="ico-eye"
+                    src={hiddenPassword ? '/icons/ico-eye close.svg' : '/icons/ico-eye.svg'}
+                  />
+                  &nbsp;
+                </button>
+                <Tooltip className="tooltip">
+                  <p>
+                    {' Votre mot de passe doit contenir au moins :'}
+                    <ul>
+                      <li>
+                        {'- 12 caractères'}
+                      </li>
+                      <li>
+                        {'- une majuscule et une minuscule'}
+                      </li>
+                      <li>
+                        {'- un chiffre'}
+                      </li>
+                      <li>
+                        {
+                          '- un caractère spécial (signe de ponctuation, symbole monétaire ou mathématique)'
+                        }
+                      </li>
+                    </ul>
+                  </p>
+                </Tooltip>
+              </div>
+              {meta.error && meta.touched && (
+                <ul
+                  className="help is-danger"
+                  id="user-email-error"
+                >
+                  <p
+                    className="help is-danger columns"
+                    id="user-email-error"
+                  >
+                    <span className="column is-narrow is-vcentered">
+                      <img
+                        alt="Attention"
+                        src="/icons/picto-warning.svg"
+                      />
+                    </span>
+                    <span className="column is-paddingless is-narrow">
+                      {meta.error}
+                    </span>
+                  </p>
+                </ul>
+              )}
+            </div>
+          )
+        }}
+      </Field>
     )
   }
 }
 
-PasswordInput.defaultProps = {
-  info: null,
-  noPasswordToggler: false
-}
-
-PasswordInput.propTypes = {
-  info: PropTypes.string,
-  noPasswordToggler: PropTypes.bool
-}
-
-export default PasswordInput
-*/
+export default Password
