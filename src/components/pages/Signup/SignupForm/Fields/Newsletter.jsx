f96b328736c3822079733c0f1170b05a6b9cd4df
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-03-12 11:11:42 +0100

    (MOB): Turn components into Field components

--- src/components/pages/Signup/SignupForm/Fields/Newsletter.jsx
+++ src/components/pages/Signup/SignupForm/Fields/Newsletter.jsx
@@ -1,23 +1,30 @@
-import React from "react"
+import React from 'react'
+import { Field } from 'react-final-form'
 
 function Newsletter() {
   return (
-    <div className="field field-checkbox">
-      <label
-        className=""
-        htmlFor="user-newsletter_ok"
-      >
-        <input
-          aria-describedby="user-newsletter_ok-error"
-          className="input"
-          id="user-newsletter_ok"
-          name="newsletter_ok"
-          type="checkbox"
-          value=""
-        />
-        {'Je souhaite recevoir les actualités du pass Culture'}
-      </label>
-    </div>
+    <Field
+      name="newsletter"
+    >
+      {({input}) => (
+        <div className="field field-checkbox">
+          <label
+            className=""
+            htmlFor="user-newsletter_ok"
+          >
+            <input
+              {...input}
+              aria-describedby="user-newsletter_ok-error"
+              className="input"
+              id="user-newsletter_ok"
+              type="checkbox"
+              value=""
+            />
+            {'Je souhaite recevoir les actualités du pass Culture'}
+          </label>
+        </div>
+      )}
+    </Field>
   )
 }
 
