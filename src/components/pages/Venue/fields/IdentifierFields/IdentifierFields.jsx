74fe01d06b21023ed49ddade8ebed158f39850f9
Romain Le Cellier <romain.le-cellier@polyconseil.fr>
2021-03-30 11:55:02 +0200

    Venue.fields.IdentifierFields.jsx: rewrite as functional component

--- src/components/pages/Venue/fields/IdentifierFields/IdentifierFields.jsx
+++ src/components/pages/Venue/fields/IdentifierFields/IdentifierFields.jsx
@@ -1,6 +1,6 @@
 import classnames from 'classnames'
 import PropTypes from 'prop-types'
-import React, { PureComponent } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { Field } from 'react-final-form'
 import { removeWhitespaces } from 'react-final-form-utils'
 import ReactTooltip from 'react-tooltip'
@@ -78,207 +78,206 @@ const venueTypeValidate = venueType => {
   return ''
 }
 
-class IdentifierFields extends PureComponent {
-  componentDidUpdate() {
+const IdentifierFields = props => {
+  const {
+    formSiret,
+    fieldReadOnlyBecauseFrozenFormSiret,
+    initialSiret,
+    isCreatedEntity,
+    readOnly,
+    venueLabels,
+    venueLabelId,
+    venueTypes,
+    venueTypeId,
+  } = props
+
+  useEffect(() => {
     ReactTooltip.rebuild()
-  }
+  }, [])
 
-  commentValidate = comment => {
-    const { formSiret } = this.props
-
-    const formatedSiret = removeWhitespaces(formSiret)
-
-    if (formatedSiret && formatedSiret.length === 14) {
+  const commentValidate = useCallback(
+    comment => {
+      const formatedSiret = removeWhitespaces(formSiret)
+      if (formatedSiret && formatedSiret.length === 14) {
+        return ''
+      }
+      if (comment === undefined || comment === '') {
+        return 'Ce champ est obligatoire'
+      }
       return ''
-    }
-    if (comment === undefined || comment === '') {
-      return 'Ce champ est obligatoire'
-    }
-    return ''
-  }
+    },
+    [formSiret]
+  )
 
-  render() {
-    const {
-      fieldReadOnlyBecauseFrozenFormSiret,
-      initialSiret,
-      isCreatedEntity,
-      readOnly,
-      venueLabels,
-      venueLabelId,
-      venueTypes,
-      venueTypeId,
-    } = this.props
+  const siretLabel = isCreatedEntity
+    ? 'SIRET du lieu qui accueille vos offres (si applicable) : '
+    : 'SIRET : '
 
-    const siretLabel = isCreatedEntity
-      ? 'SIRET du lieu qui accueille vos offres (si applicable) : '
-      : 'SIRET : '
+  const venueTypeLabel = getLabelFromList(venueTypes, venueTypeId)
+  const venueLabelText = getLabelFromList(venueLabels, venueLabelId)
 
-    const venueTypeLabel = getLabelFromList(venueTypes, venueTypeId)
-    const venueLabelText = getLabelFromList(venueLabels, venueLabelId)
-
-    return (
-      <div className="section identifier-field-section">
-        <h2 className="main-list-title">
-          {'Informations lieu'}
-          {!readOnly && (
-            <span className="required-fields-hint">
-              {'Les champs marqués d’un'}
-              <span className="required-legend">
-                {' * '}
-              </span>
-              {' sont obligatoires'}
+  return (
+    <div className="section identifier-field-section">
+      <h2 className="main-list-title">
+        {'Informations lieu'}
+        {!readOnly && (
+          <span className="required-fields-hint">
+            {'Les champs marqués d’un'}
+            <span className="required-legend">
+              {' * '}
             </span>
-          )}
-        </h2>
-        <div className="field-group">
-          {isCreatedEntity && <HiddenField name="managingOffererId" />}
-          <TextField
-            format={formatSiret}
-            label={siretLabel}
-            name="siret"
-            parse={parseSiret}
-            readOnly={readOnly || initialSiret !== null}
-            renderValue={handleSiretRenderValue(fieldReadOnlyBecauseFrozenFormSiret, readOnly)}
-            type="siret"
-            validate={initialSiret ? undefined : siretValidate}
-          />
-          <TextField
-            label="Nom du lieu : "
-            name="name"
-            readOnly={readOnly || fieldReadOnlyBecauseFrozenFormSiret}
-            required
-          />
-          <TextField
-            label="Nom d’usage du lieu : "
-            name="publicName"
-            readOnly={readOnly}
-          />
-          <TextField
-            label="E-mail : "
-            name="bookingEmail"
-            readOnly={readOnly}
-            renderValue={handleBookingEmailRenderValue(readOnly)}
-            required
-            type="email"
-          />
-          <TextareaField
-            label="Commentaire (si pas de SIRET) : "
-            name="comment"
-            readOnly={readOnly}
-            rows={1}
-            validate={this.commentValidate}
-          />
-          <div
-            className={classnames('field field-select is-label-aligned', {
-              readonly: readOnly,
-            })}
-          >
-            <div className="field-label">
-              <label htmlFor="venue-type">
-                {'Type de lieu : '}
-              </label>
-              <span className="field-asterisk">
-                {'*'}
-              </span>
-            </div>
-
-            <div className="field-control">
-              {!readOnly ? (
-                <div className="control control-select">
-                  <div
-                    className={classnames('select', {
-                      readonly: readOnly,
-                    })}
-                  >
-                    <Field
-                      component="select"
-                      disabled={readOnly}
-                      id="venue-type"
-                      name="venueTypeId"
-                      required
-                      validate={venueTypeValidate}
-                    >
-                      <option value="">
-                        {'Choisissez un type de lieu dans la liste'}
-                      </option>
-                      {venueTypes.map(venueType => (
-                        <option
-                          key={`venue-type-${venueType.id}`}
-                          value={venueType.id}
-                        >
-                          {venueType.label}
-                        </option>
-                      ))}
-                    </Field>
-                  </div>
-                </div>
-              ) : (
-                <div
-                  className="venue-type-label"
-                  id="venue-type"
-                >
-                  <span>
-                    {venueTypeLabel}
-                  </span>
-                </div>
-              )}
-            </div>
+            {' sont obligatoires'}
+          </span>
+        )}
+      </h2>
+      <div className="field-group">
+        {isCreatedEntity && <HiddenField name="managingOffererId" />}
+        <TextField
+          format={formatSiret}
+          label={siretLabel}
+          name="siret"
+          parse={parseSiret}
+          readOnly={readOnly || initialSiret !== null}
+          renderValue={handleSiretRenderValue(fieldReadOnlyBecauseFrozenFormSiret, readOnly)}
+          type="siret"
+          validate={initialSiret ? undefined : siretValidate}
+        />
+        <TextField
+          label="Nom du lieu : "
+          name="name"
+          readOnly={readOnly || fieldReadOnlyBecauseFrozenFormSiret}
+          required
+        />
+        <TextField
+          label="Nom d’usage du lieu : "
+          name="publicName"
+          readOnly={readOnly}
+        />
+        <TextField
+          label="E-mail : "
+          name="bookingEmail"
+          readOnly={readOnly}
+          renderValue={handleBookingEmailRenderValue(readOnly)}
+          required
+          type="email"
+        />
+        <TextareaField
+          label="Commentaire (si pas de SIRET) : "
+          name="comment"
+          readOnly={readOnly}
+          rows={1}
+          validate={commentValidate}
+        />
+        <div
+          className={classnames('field field-select is-label-aligned', {
+            readonly: readOnly,
+          })}
+        >
+          <div className="field-label">
+            <label htmlFor="venue-type">
+              {'Type de lieu : '}
+            </label>
+            <span className="field-asterisk">
+              {'*'}
+            </span>
           </div>
-          <div
-            className={classnames('field field-select is-label-aligned', {
-              readonly: readOnly,
-            })}
-          >
-            <div className="field-label">
-              <label htmlFor="venue-label">
-                {'Label du lieu :'}
-              </label>
-            </div>
 
-            <div className="field-control">
-              {!readOnly ? (
-                <div className="control control-select">
-                  <div
-                    className={classnames('select', {
-                      readonly: readOnly,
-                    })}
-                  >
-                    <Field
-                      component="select"
-                      disabled={readOnly}
-                      id="venue-label"
-                      name="venueLabelId"
-                    >
-                      <option value="">
-                        {'Choisissez un label dans la liste'}
-                      </option>
-                      {venueLabels.map(venueLabel => (
-                        <option
-                          key={`venue-label-${venueLabel.id}`}
-                          value={venueLabel.id}
-                        >
-                          {venueLabel.label}
-                        </option>
-                      ))}
-                    </Field>
-                  </div>
-                </div>
-              ) : (
+          <div className="field-control">
+            {!readOnly ? (
+              <div className="control control-select">
                 <div
-                  className="venue-label-label"
-                  id="venue-label"
+                  className={classnames('select', {
+                    readonly: readOnly,
+                  })}
                 >
-                  <span>
-                    {venueLabelText}
-                  </span>
+                  <Field
+                    component="select"
+                    disabled={readOnly}
+                    id="venue-type"
+                    name="venueTypeId"
+                    required
+                    validate={venueTypeValidate}
+                  >
+                    <option value="">
+                      {'Choisissez un type de lieu dans la liste'}
+                    </option>
+                    {venueTypes.map(venueType => (
+                      <option
+                        key={`venue-type-${venueType.id}`}
+                        value={venueType.id}
+                      >
+                        {venueType.label}
+                      </option>
+                    ))}
+                  </Field>
                 </div>
-              )}
-            </div>
+              </div>
+            ) : (
+              <div
+                className="venue-type-label"
+                id="venue-type"
+              >
+                <span>
+                  {venueTypeLabel}
+                </span>
+              </div>
+            )}
+          </div>
+        </div>
+        <div
+          className={classnames('field field-select is-label-aligned', {
+            readonly: readOnly,
+          })}
+        >
+          <div className="field-label">
+            <label htmlFor="venue-label">
+              {'Label du lieu :'}
+            </label>
+          </div>
+
+          <div className="field-control">
+            {!readOnly ? (
+              <div className="control control-select">
+                <div
+                  className={classnames('select', {
+                    readonly: readOnly,
+                  })}
+                >
+                  <Field
+                    component="select"
+                    disabled={readOnly}
+                    id="venue-label"
+                    name="venueLabelId"
+                  >
+                    <option value="">
+                      {'Choisissez un label dans la liste'}
+                    </option>
+                    {venueLabels.map(venueLabel => (
+                      <option
+                        key={`venue-label-${venueLabel.id}`}
+                        value={venueLabel.id}
+                      >
+                        {venueLabel.label}
+                      </option>
+                    ))}
+                  </Field>
+                </div>
+              </div>
+            ) : (
+              <div
+                className="venue-label-label"
+                id="venue-label"
+              >
+                <span>
+                  {venueLabelText}
+                </span>
+              </div>
+            )}
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 IdentifierFields.defaultProps = {
