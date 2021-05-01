bfaa209b1abda2a675550318bbf08a1b2e3108e6
apibrac <alexis.pibrac@gmail.com>
2021-05-01 10:00:03 +0200

    Correctly pre-parse urls

--- src/components/pages/Signin/TestImport.jsx
+++ src/components/pages/Signin/TestImport.jsx
@@ -1,20 +1,8 @@
 import React from 'react'
 
-const NEW_LINE = `
-`
+import urls from './urls.json'
 
 async function fetchCSVAndTestImports() {
-  const response = await fetch('/thumb_upload_logs.csv')
-  const reader = response.body.getReader()
-  const file = await reader.read()
-
-  const decoder = new TextDecoder('utf-8')
-  const csv = decoder.decode(file.value)
-
-  const urls = csv.split(NEW_LINE).map(line => line.split(',')[3])
-  urls.shift()
-  urls.pop()
-
   for await (const url of urls) {
     console.log(url)
     await fetch(url)
