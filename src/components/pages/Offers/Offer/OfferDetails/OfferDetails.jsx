0ebb59e8d52c26f706d61f5453b33d30a5b37296
damienSchPassCulture <damien.schaller@passculture.app>
2021-09-23 16:05:44 +0200

    (PC-10534) : change offer id by nonhumanized id offers

--- src/components/pages/Offers/Offer/OfferDetails/OfferDetails.jsx
+++ src/components/pages/Offers/Offer/OfferDetails/OfferDetails.jsx
@@ -214,6 +214,7 @@ const OfferDetails = ({
             {offer ? (
               <OfferPreviewLink
                 mediationId={offer.activeMediation ? offer.activeMediation.id : null}
+                nonHumanizedOfferId={offer.nonHumanizedId}
                 offerId={offer.id}
               />
             ) : null}
